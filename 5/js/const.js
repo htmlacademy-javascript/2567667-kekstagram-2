3ea76bb4040067b2d60a025db19159f5a0321351
Keks <keks@htmlacademy.ru>
2024-12-24 18:34:28 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/const.js
@@ -0,0 +1,44 @@
+const MIN_ID_VALUE = 1;
+const MAX_ID_VALUE = 1000;
+
+const MIN_AVATAR_VALUE = 1;
+const MAX_AVATAR_VALUE = 6;
+
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+
+const MIN_COMMENTS_COUNT = 0;
+const MAX_COMMENTS_COUNT = 30;
+
+const MIN_MESSAGE_COUNT = 1;
+const MAX_MESSAGE_COUNT = 2;
+
+const PHOTOS_COUNT = 25;
+
+const DESCRIPTIONS = [
+  'Закат на пляже',
+  'Вечеринка в горах',
+  'Красивая природа',
+  'Городская жизнь',
+  'Путешествие мечты',
+];
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота.',
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+];
+
+const NAMES = ['Иван', 'Мария', 'Александр', 'Екатерина', 'Сергей', 'Юлия'];
+
+export {
+  MIN_ID_VALUE, MAX_ID_VALUE,
+  MIN_AVATAR_VALUE, MAX_AVATAR_VALUE,
+  MIN_LIKES, MAX_LIKES,
+  MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT,
+  MIN_MESSAGE_COUNT, MAX_MESSAGE_COUNT,
+  PHOTOS_COUNT, DESCRIPTIONS, COMMENTS, NAMES
+};
---
+++ 5/js/functions.js
@@ -0,0 +1,51 @@
+// Функция для проверки длины строки
+function checkStringLength(str, maxLength) {
+  return str.length <= maxLength;
+}
+
+// Функция для проверки, является ли строка палиндромом
+function isPalindrome(str) {
+  let cleanedStr = str.replace(/\s+/g, '').toLowerCase();
+  return cleanedStr === cleanedStr.split('').reverse().join('');
+}
+
+function extractDigits(input) {
+  let str = input.toString();
+
+  let digits = '';
+  for (let char of str) {
+      if (!isNaN(parseInt(char))) {
+          digits += char;
+      }
+  }
+
+  if (digits === '') {
+      return NaN;
+  }
+
+  return parseInt(digits, 10);
+}
+
+
+// Функция "Делу — время"
+function isMeetingWithinWorkday(workStart, workEnd, meetingStart, meetingDuration) {
+  function timeTiMinutes(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+
+
+  const workStartMinutes = timeToMinutes(workStart);
+  const workEndMinutes = timeToMinutes(workEnd);
+  const meetingStartMinutes = timeToMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  return meetingStartMinutes >= workStartMinutes && meetingEndMinutes <= workEndMinutes;
+
+}
+
+isMeetingWithinWorkday('08:00', '17:30', '14:00', 90); // true
+isMeetingWithinWorkday('8:0', '10:0', '8:0', 120);     // true
+isMeetingWithinWorkday('08:00', '14:30', '14:00', 90); // false
+isMeetingWithinWorkday('14:00', '17:30', '08:0', 90);  // false
+isMeetingWithinWorkday('8:00', '17:30', '08:00', 900); // false
