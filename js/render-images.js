d58aafe6f8c4e4bd6bd2108e3b00eca807b7ec6d
Amal <amal.gabidov@mail.ru>
2025-01-26 22:40:52 +0300

    fixed description display

--- js/render-images.js
+++ js/render-images.js
@@ -7,14 +7,15 @@ const errorTemplate = document.querySelector('#data-error').content.querySelecto
 const renderImages = (photos) => {
   const fragment = document.createDocumentFragment();
 
-  photos.forEach(({ url, likes, comments }) => {
+  photos.forEach(({ url, description, likes, comments }) => {
     const pictureElement = pictureTemplate.cloneNode(true);
     pictureElement.querySelector('.picture__img').src = url;
     pictureElement.querySelector('.picture__likes').textContent = likes;
     pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture__img').alt = description;
 
     pictureElement.addEventListener('click', () => {
-      openBigPicture({ url, likes, comments });
+      openBigPicture({ url, description, likes, comments });
     });
 
     fragment.appendChild(pictureElement);
