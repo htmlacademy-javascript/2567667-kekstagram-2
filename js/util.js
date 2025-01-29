6476bba2a59ae2f2ff42ba42d929e0cda4a72b89
Amal <amal.gabidov@mail.ru>
2025-01-29 11:02:00 +0300

    refactor: remove unused modules

--- js/util.js
+++ js/util.js
@@ -1,14 +1,3 @@
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
-};
-
-const getRandomArrayElement = (elements) =>
-  elements[getRandomInteger(0, elements.length - 1)];
-
-export { getRandomInteger, getRandomArrayElement };
-
 const debounce = (callback, delay = 500) => {
   let timeout;
   return (...args) => {
